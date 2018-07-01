2639051729a466e389031fd50df1439a19d87b54
randallkanna <randallkanna@gmail.comm>
2018-07-01 15:54:41 -0700

    organizes tests

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,28 +6,34 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
-    // Test for failing conditions in this contracts
+  function beforeEach() {
+    SupplyChain chain = SupplyChain(DeployedAddresses.SupplyChain());
+  }
 
-    // test that every modifier is working
+  /* uint numberOfElements = dummyContract.getNumberOfElements(); */
+  /* Assert.equal(numberOfElements, 0, "Number of elements should be 0"); */
 
-    // buyItem
-    /* it("should allow a user to buy an item") */
+  /* MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin()); */
+  /* Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");  } */
 
+  // Test for failing conditions in this contracts
+  // test that every modifier is working
+
+  testBuyItem() {
     // test for failure if user does not send enough funds
     // test for purchasing an item that is not for Sale
 
+  }
 
-    // shipItem
-
+  test testShipItem() {
     // test for calls that are made by not the seller
     // test for trying to ship an item that is not marked Sold
 
-    // receiveItem
+  }
 
+  testReceiveItem() {
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
 
-
-
-
+  }
 }
