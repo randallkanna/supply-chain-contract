1fb2bbe0bb47aec5fcd30517d8936bc17a433747
randallkanna <randallkanna@gmail.comm>
2018-07-02 13:33:49 -0700

    adding test testBuyItemWithInsufficientFunds

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,33 +5,39 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
-
-  function beforeEach() {
-    SupplyChain chain = SupplyChain(DeployedAddresses.SupplyChain());
-  }
+  SupplyChain chain = SupplyChain(DeployedAddresses.SupplyChain());
 
   /* uint numberOfElements = dummyContract.getNumberOfElements(); */
   /* Assert.equal(numberOfElements, 0, "Number of elements should be 0"); */
-
   /* MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin()); */
   /* Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");  } */
 
-  // Test for failing conditions in this contracts
-  // test that every modifier is working
+  // TO DO - Break out into helper functions
 
-  testBuyItem() {
+  function testBuyItemWithInsufficientFunds() public {
     // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+    chain.addItem("test", 1 ether);
+
+    /* chain.buyItem(address(chain), 1); */
+
+    // asser that it fails
+    /* Asssert.isFalse(result, false, ''); */
+    /* Assert.equal(mycontract.mynumber(), expected, "First number set should be 24."); */
 
   }
 
-  test testShipItem() {
+  // test for purchasing an item that is not for Sale
+  function testNonPurchasableItem() {
+
+  }
+
+  function testShipItem() {
     // test for calls that are made by not the seller
     // test for trying to ship an item that is not marked Sold
 
   }
 
-  testReceiveItem() {
+  function testReceiveItem() {
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
 
